4dc5295127b45a8d606c7147afb45d69594480be
dean <64692815+deant-digital@users.noreply.github.com>
2026-02-10 12:00:00 +0800

    feat: add 41 First Missing Positive

---
+++ 0041_first_missing_positive.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+ public:
+  int firstMissingPositive(vector<int>& nums) {
+    int n(nums.size());
+    for (int i{}; i < n; ++i) {
+      while (nums[i] >= 1 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+        swap(nums[i], nums[nums[i] - 1]);
+      }
+    }
+    for (int i{}; i < n; ++i) {
+      if (nums[i] != i + 1) {
+        return i + 1;
+      }
+    }
+    return n + 1;
+  }
+};
