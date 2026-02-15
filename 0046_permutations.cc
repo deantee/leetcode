0828e43738e7cc108bbc9763f87139416253863b
dean <64692815+deant-digital@users.noreply.github.com>
2026-02-15 12:00:00 +0800

    feat: add 46 Permutations

---
+++ 0046_permutations.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+ public:
+  vector<vector<int>> permute(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> ans;
+    do {
+      ans.emplace_back(nums);
+    } while (next_permutation(nums.begin(), nums.end()));
+    return ans;
+  }
+};
