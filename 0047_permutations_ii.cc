116ecf6f7dfacc78781c18ce4e3813286bf46d4c
dean <64692815+deant-digital@users.noreply.github.com>
2026-02-16 12:00:00 +0800

    feat: add 47 Permutations II

---
+++ 0047_permutations_ii.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+ public:
+  vector<vector<int>> permuteUnique(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> ans;
+    do {
+      ans.emplace_back(nums);
+    } while (next_permutation(nums.begin(), nums.end()));
+    return ans;
+  }
+};
