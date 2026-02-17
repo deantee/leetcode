8b019ebe743d5f44c5a2d7cadb956faafb17ceb2
dean <64692815+deant-digital@users.noreply.github.com>
2026-02-17 12:00:00 +0800

    feat: add 48 Rotate Image

---
+++ 0048_rotate_image.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+ public:
+  void rotate(vector<vector<int>>& matrix) {
+    int n(matrix.size());
+    for (int i{}; i < n; ++i) {
+      for (int j{i + 1}; j < n; ++j) {
+        swap(matrix[i][j], matrix[j][i]);
+      }
+    }
+    for (int i{}; i < n; ++i) {
+      for (int j{}; j < n / 2; ++j) {
+        swap(matrix[i][j], matrix[i][n - j - 1]);
+      }
+    }
+  }
+};
