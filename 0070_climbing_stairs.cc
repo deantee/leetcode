a27ec15c3b3515d89508feff6e6e6dc3ea7b8319
dean <64692815+deant-digital@users.noreply.github.com>
2026-03-11 12:00:00 +0800

    feat: add 70 Climbing Stairs

---
+++ 0070_climbing_stairs.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+ public:
+  int climbStairs(int n) {
+    if (n < 2) {
+      return 1;
+    }
+    int a{1}, b{1};
+    while (n-- > 1) {
+      int c{a + b};
+      a = b;
+      b = c;
+    }
+    return b;
+  }
+};
